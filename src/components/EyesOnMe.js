b48a3fe596e9726317a639416754a2ff46c1d237
Alan Silva <silzen.ent@gmail.com>
2024-01-18 11:29:09 -0700

    Completed Event Handling Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+function EyesOnMe(){
+
+    function focusEvent(){
+        console.log('Good!')
+    }
+
+    function blurEvent(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+    <button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>
+    );
+}
+
+export default EyesOnMe
+
+
